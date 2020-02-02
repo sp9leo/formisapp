211744ed5d5dd3b57c64fffa117b129753bd7098
sp9leo <sp9leonardo@gmail.com>
2020-02-02 20:22:08 +0100

    Update hooks.py

--- formisapp/hooks.py
+++ formisapp/hooks.py
@@ -15,8 +15,8 @@ fixtures = ["Custom Script", "Print Format"]
 # Includes in <head>
 # ------------------
 
-# include js, css files in header of desk.html
- app_include_css = "/assets/formisapp/css/formisapp.css"
+# include js, css files in header of desk.htm
+app_include_css = "/assets/formisapp/css/formisapp.css"
 # app_include_js = "/assets/formisapp/js/formisapp.js"
 
 # include js, css files in header of web template

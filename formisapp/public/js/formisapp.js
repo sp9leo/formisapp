e9732cd5fa63054d26c0165404f4168d3546761e
sp9leo <sp9leonardo@gmail.com>
2020-06-24 22:20:15 +0200

    Create formisapp.js
    Address Dialog update

---
+++ formisapp/public/js/formisapp.js
@@ -0,0 +1,90 @@
+/*customer quick entry*/
+
+frappe.provide('frappe.ui.form');
+
+frappe.ui.form.CustomerQuickEntryForm = frappe.ui.form.QuickEntryForm.extend({
+	init: function(doctype, after_insert) {
+		this.skip_redirect_on_error = true;
+		this._super(doctype, after_insert);
+	},
+
+	render_dialog: function() {
+		this.mandatory = this.mandatory.concat(this.get_variant_fields());
+		this._super();
+	},
+
+	get_variant_fields: function() {
+		var variant_fields = [{
+			fieldtype: "Section Break",
+			label: __("Primary Contact Details"),
+			collapsible: 1
+		},
+		{
+			label: __("Email Id"),
+			fieldname: "email_id",
+			fieldtype: "Data"
+		},
+		{
+			fieldtype: "Column Break"
+		},
+		{
+			label: __("Mobile Number"),
+			fieldname: "mobile_no",
+			fieldtype: "Data"
+		},
+		{
+			fieldtype: "Section Break",
+			label: __("Primary Address Details"),
+			collapsible: 1
+		},
+		{
+			label: __("Address Line 1"),
+			fieldname: "address_line1",
+			fieldtype: "Data"
+		},
+		{
+			label: __("Address Line 2"),
+			fieldname: "address_line2",
+			fieldtype: "Data"
+		},
+		{
+			label: __("Pošta"),
+			fieldname: "pincode",
+			fieldtype: "Link",
+			options: "PostneStevilke"
+			
+		},
+		{
+			fieldtype: "Column Break"
+		},
+		{
+			label: __("Kraj"),
+			fieldname: "city",
+			fieldtype: "Data"
+		},
+		{
+			label: __("State"),
+			fieldname: "state",
+			fieldtype: "Data"
+		},
+		{
+			label: __("Država"),
+			fieldname: "country",
+			fieldtype: "Link",
+            options: "Country",
+            default: "Slovenia"
+		},
+		{
+			label: __("Customer POS Id"),
+			fieldname: "customer_pos_id",
+			fieldtype: "Data",
+			hidden: 1
+		}];
+
+        return variant_fields;
+	},
+    
+    
+
+    
+	});

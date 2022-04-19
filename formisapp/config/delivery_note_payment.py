466b824189ffd10819cbe05b577e2ddc97a0066f
sp9leo <sp9leonardo@gmail.com>
2022-04-19 09:36:08 +0200

    New branch
    Added erpcustom to new branch

--- formisapp/config/delivery_note_payment.py
+++
@@ -1,6 +0,0 @@
-@frappe.whitelist()
-def make_payment_entry():
-    payment_entry = frappe.new_doc("Payment Entry")
-    payment_entry.party_type = "Supplier"
-    payment_entry.save()
-    return payment_entry.name

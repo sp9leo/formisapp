e45a660dfe511780b0b4942735884a385cab68ca
sp9leo <sp9leonardo@gmail.com>
2022-06-02 13:05:54 +0200

    Revert "master to 2022"

---
+++ formisapp/formis/doctype/delovni_nalog_interni/delovni_nalog_interni.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, formis and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Delovni Nalog Interni', {
+	refresh: function(frm) {
+
+	}
+});
